66e832dd9fc48446ba66f37099ce66db7461ecc7
ildd <k.e.andlou@gmail.com>
2025-05-24 00:01:08 +0300

    факториалы

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  // Handle special cases
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  
+  let result = 1;
+  // Multiply all numbers from n down to 1
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  
+  return result;
 }
